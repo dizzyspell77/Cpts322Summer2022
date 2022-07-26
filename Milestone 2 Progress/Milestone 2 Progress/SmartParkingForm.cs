8a5e65e1ab1a675cac9a99dfdf338cd220bdc90c
dizzyspell77 <dazniel.zepeda@wsu.edu>
2022-07-25 19:26:14 -0700

    Trilateration class

--- Milestone 2 Progress/Milestone 2 Progress/SmartParkingForm.cs
+++ Milestone 2 Progress/Milestone 2 Progress/SmartParkingForm.cs
@@ -104,6 +104,28 @@ namespace Milestone_2_Progress
                         //int index = int(Math.Floor(p.x / 1.5) + 6);
                     }
 
+                    var child = client.Child("Beacons/data");
+                    var observable = child.AsObservable<Beacon>();
+                    var r1 = 0;
+                    var r2 = 0;
+                    var r3 = 0;
+                    var x1 = 0;
+                    var y1 = 5;
+                    var x2 = 9;
+                    var y2 = 1.5;
+                    var x3 = 0;
+                    var y3 = 7;
+                    var A = 2 * x2 - 2 * x1;
+                    var B = 2 * y2 - 2 * y1;
+                    var C = Math.Pow(r1, 2) - Math.Pow(r2, 2) - Math.Pow(x1, 2) + Math.Pow(x2, 2) - Math.Pow(y1, 2) + Math.Pow(y2, 2);
+                    var D = 2 * x3 - 2 * y2;
+                    var E = 2 * y3 - 2 * y2;
+                    var F = Math.Pow(r2, 2) - Math.Pow(r3, 2) - Math.Pow(x2, 2) + Math.Pow(x3, 2) - Math.Pow(y2, 2) + Math.Pow(y3, 2);
+                    var x = (C * E - F * B) / (E * A - B * D);
+                    var y = (C * D - A * F) / (B * D - A * E);
+                    var xResult = x;
+                    var yResult = y;
+
 
                 });
 
